dbd7bc2c89c20e6cb540b6bdfec98ffc03ee667c
MAGAMBO USERBOT <93328169+BHAGWANUSERBOT@users.noreply.github.com>
2022-02-28 19:53:51 +0530

    Add files via upload

---
+++ userbot/modules/space.py
@@ -0,0 +1,13 @@
+from userbot import bot
+from userbot.utils import admin_cmd
+from userbot import CMD_HELP
+
+@bot.on(admin_cmd(pattern=r"space"))
+async def space(e):
+    await e.edit("ㅤ")
+@bot.on(admin_cmd(pattern=r"blank"))
+async def blank(e):
+    await e.edit("­")
+
+# Ported from shivam's project
+CMD_HELP.update({"space": "try .space or .blank"})
